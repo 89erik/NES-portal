0d3e978e016698c0ea680661814e1d5ea8fb38a0
Erik Lothe <eriklothe@gmail.com>
2017-01-29 12:50:16 +0100

    updates test framework

--- tests/test_hooks.asm
+++ tests/test_hooks.asm
@@ -2,3 +2,7 @@ TestSignedIsNegative:               JSR SignedIsNegative
 TestDivide:                         JSR Divide
 TestMultiply:                       JSR Multiply
 TestAccumulateLong:                 JSR AccumulateLong
+TestSignedIncrease:                 JSR SignedIncrease
+TestSignedDecrease:                 JSR SignedDecrease
+TestMultiplyLong:                   JSR MultiplyLong
+TestAddLong:                        JSR AddLong
