4f5bb45593e972f09dccda52ad5d62d59cb1dffd
Erik Lothe <eriklothe@gmail.com>
2017-01-29 15:10:47 +0100

    tests with negative numbers

--- execute_tests.sh
+++ execute_tests.sh
@@ -1,11 +1,16 @@
 #/bin/bash
 
+RC=0
 for test_file in `ls tests/*.test`; do
-   test_executor/6502_tester portal.nes symbols.txt $test_file  > /dev/null
-   if [[ $? -eq 0 ]]; then
+    msg=$(test_executor/6502_tester portal.nes symbols.txt $test_file 2>&1 >/dev/null)
+    if [[ $? -eq 0 ]]; then
        echo -e "  \e[32m[PASS]\e[0m    $(basename $test_file .test)"
-   else
+    else
        echo -e "  \e[31m[FAIL]\e[0m    $(basename $test_file .test)"
-   fi
-
+       while read -r line; do
+               echo "                $line"
+           done <<< "$msg"
+       RC=1
+    fi
 done
+exit $RC
